141cc07680c086990e5df84af907a25036b304a1
Elizabeth Chan <elizabeth.chan@tulip.io>
2017-06-29 20:32:15 -0400

    Used fetch to retrieve Pokemon.

--- src/App.js
+++ src/App.js
@@ -2,74 +2,85 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemon: null,
-      error: false,
-      textInput: '',
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemon: null,
+            error: false,
+            textInput: '',
+        }
     }
-  }
-  render() {
-    const { pokemon, error, textInput } = this.state;
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h1>Pokemon Search</h1>
-          <h2>Gotta fetch 'em all!</h2>
-          <div className="pokeball">
-            <div className="pokeball-bottom" />
-            <div className="pokeball-band" />
-            <div className="pokeball-button" />
-          </div>
-        </div>
-        <div className="content-container">
-          <h3>Enter a number to search for a pokemon</h3>
-          <div className="input-group">
-            <input
-              value={textInput}
-              type="number"
-              className="input"
-              onChange={ev => this.setState({ textInput: ev.target.value, error: false })}
-            />
-            <button
-              className="submit-button"
-              onClick={this.getPokemon}
-            >
-              Search Pokemon
-            </button>
-          </div>
-          {
-            error ?
-              <div className="error-container">
-                <h3>Oops! something went wrong. Please try again</h3>
-              </div> : null
-          }
-          {
-            pokemon ?
-              <div className="pokemon-info-card">
-                <h3>You chose</h3>
-                <h1>{pokemon.name}</h1>
-                <img
-                  src={pokemon.sprites.front_default}
-                  alt="pokemon-sprite"
-                  className="pokemon-image"
-                />
-              </div> : null
-          }
-        </div>
-      </div>
-    );
-  }
 
-  getPokemon = () => {
-    const { textInput } = this.state;
-    if (textInput.length) {
-      const url = `http://pokeapi.co/api/v2/pokemon/${textInput}`;
-
-      //TODO: Add fetch code below
+    render() {
+        const {pokemon, error, textInput} = this.state;
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Pokemon Search</h1>
+                    <h2>Gotta fetch 'em all!</h2>
+                    <div className="pokeball">
+                        <div className="pokeball-bottom"/>
+                        <div className="pokeball-band"/>
+                        <div className="pokeball-button"/>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <h3>Enter a number to search for a pokemon</h3>
+                    <div className="input-group">
+                        <input
+                            value={textInput}
+                            type="number"
+                            className="input"
+                            onChange={ev => this.setState({textInput: ev.target.value, error: false})}
+                        />
+                        <button
+                            className="submit-button"
+                            onClick={this.getPokemon}
+                        >
+                            Search Pokemon
+                        </button>
+                    </div>
+                    {
+                        error ?
+                            <div className="error-container">
+                                <h3>Oops! something went wrong. Please try again</h3>
+                            </div> : null
+                    }
+                    {
+                        pokemon ?
+                            <div className="pokemon-info-card">
+                                <h3>You chose</h3>
+                                <h1>{pokemon.name}</h1>
+                                <img
+                                    src={pokemon.sprites.front_default}
+                                    alt="pokemon-sprite"
+                                    className="pokemon-image"
+                                />
+                            </div> : null
+                    }
+                </div>
+            </div>
+        );
     }
-  }
+
+    getPokemon = () => {
+        const {textInput} = this.state;
+        if (textInput.length) {
+            const url = `http://pokeapi.co/api/v2/pokemon/${textInput}`;
+            return fetch(url)
+                .then(res => res.json())
+                .then(res => this.setState({pokemon: res}))
+                .catch(data => this.setState(
+                    {
+                        pokemon: false,
+                        error: true
+                    })
+                );
+            //TODO: proper error handling
+        }
+    }
+
 }
+
 
 export default App;
